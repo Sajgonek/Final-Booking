e9eb85e3a30a303d5a8aab3b05527456d0666781
Sajgonek <34008096+Sajgonek@users.noreply.github.com>
2017-12-09 18:43:59 +0100

    Added Id to customer

---
+++ PZOHFinal/PZOHFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PZOHFinal.MvcApplication" Language="C#" %>
---
+++ PZOHFinal/PZOHFinal/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PZOHFinal.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+    }
+}
